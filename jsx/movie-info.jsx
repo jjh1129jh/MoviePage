db7e646f1df360ebc74786613180c6f3caf95eef
jjh1129jh <jjh1129jh@naver.com>
2026-03-08 18:50:03 +0900

    movie.homepage 오타 수정

--- jsx/movie-info.jsx
+++ jsx/movie-info.jsx
@@ -15,7 +15,7 @@ export default async function MovieInfo({ id }) {
                 <h1 className={css.title}>{movie.title}</h1>
                 <h3>⭐️{movie.vote_average.toFixed(1)}</h3>
                 <p>{movie.overview}</p>
-                <a href="movie.homepage" target={"_blank"}>Homepage</a>
+                <a href={movie.homepage} target={"_blank"}>Homepage</a>
             </div>
         </div>
     )
